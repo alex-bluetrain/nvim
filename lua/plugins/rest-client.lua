03701676b21eae5e795e3df3690603c51252ecca
Alex Verstraeten <averstraeten@gmail.com>
2025-03-23 13:00:56 -0300

    4 spaces lua

--- lua/plugins/rest-client.lua
+++ lua/plugins/rest-client.lua
@@ -1,17 +1,17 @@
 return {
-  "rest-nvim/rest.nvim",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      table.insert(opts.ensure_installed, "http")
+    "rest-nvim/rest.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            opts.ensure_installed = opts.ensure_installed or {}
+            table.insert(opts.ensure_installed, "http")
+        end,
+    },
+    config = function()
+        -- local rest_nvim = require("rest-nvim")
+        vim.keymap.set("n", "<leader>rr", ":Rest run<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>rp", ":Rest preview<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>ro", ":Rest open<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>rl", ":Rest logs<CR>", { noremap = true, silent = true })
     end,
-  },
-  config = function()
-    -- local rest_nvim = require("rest-nvim")
-    vim.keymap.set("n", "<leader>rr", ":Rest run<CR>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>rp", ":Rest preview<CR>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>ro", ":Rest open<CR>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>rl", ":Rest logs<CR>", { noremap = true, silent = true })
-  end,
 }
