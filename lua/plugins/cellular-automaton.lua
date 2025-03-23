1777f2ff17257b43f200f86d53caa23a9fa10050
Alex Verstraeten <averstraeten@gmail.com>
2025-03-23 13:02:07 -0300

    minor changes

--- lua/plugins/cellular-automaton.lua
+++ lua/plugins/cellular-automaton.lua
@@ -2,7 +2,7 @@ return {
     "eandrju/cellular-automaton.nvim",
     cmd = { "CellularAutomaton" },
     keys = {
-        { "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Start Make It Rain Animation" },
+        { "<leader>cZ", "<cmd>CellularAutomaton make_it_rain<CR>", desc = "Start Make It Rain Animation" },
     },
     config = function()
         -- Optional configuration can go here if needed
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,8 @@
 -- falta revisar como funciona y configurarlo
+if true then
+    return {}
+end
+
 return {
     "tpope/vim-fugitive",
     config = function() end,
